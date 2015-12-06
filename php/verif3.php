1284e1cda746a5b3d420688386b03e22907baa9d
Tmichels <thomas.michels@eleves.ec-nantes.fr>
2015-12-06 15:42:39 +0100

    verif3.php

--- php/verif3.php
+++ php/verif3.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $email = $_POST['email'];
 $mdp = $_POST['mdp'];
 $mdp2 = $_POST['mdp2'];
@@ -23,12 +24,14 @@ $nomAcc = $_POST['nomAcc'];
 $train = $_POST['train'];
 $traindate = $_POST['traindate'];
 $trainheure = $_POST['trainheure'];
+
+
     
 
 if (empty($fClub) or empty($fDistrict)) {
     include("inscriptionnew3.php");
 } else {
-    $bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=lion;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=lion;charset=utf8', 'root', 'lion');
     $req0 = $bdd->prepare('INSERT INTO Person (Person_Lastname, Person_Firstname) VALUES (:nom,:prenom)');
     $req0->execute(array(
         'nom' => "$nom",
@@ -49,7 +52,82 @@ if (empty($fClub) or empty($fDistrict)) {
     $req3 = $bdd->prepare('INSERT INTO Follower (Person_ID) VALUE (:id)');
     $req3->execute(array(
         'id' => "$personID",));
+        
+        
+    $req4 = $bdd->prepare("SELECT Club_ID FROM Club WHERE (Club_Name = '$club')", array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+    $req4->execute(array());
+        $row = $req4 -> fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT);
+        $clubID = $row["Club_ID"]; 
+        
+    $req5 = $bdd->prepare("SELECT District_ID FROM District WHERE (Club_Name = '$district')", array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+    $req5->execute(array());
+        $row = $req5 -> fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT);
+        $districtID = $row["District_ID"]; 
+        
+        
+        
+	$req6 = $bdd->prepare("SELECT Follower_ID FROM Follower WHERE (Person_ID = '$personID')", array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+    $req6->execute(array());
+        $row = $req6 -> fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT);
+        $followerID = $row["Follower_ID"];
+        
+        
+    $req7 = $bdd->prepare("SELECT Person_ID FROM Person WHERE (Person_Lastname = '$nom' AND Person_Firstname = '$prenom')", array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+    $req7->execute(array());
+        $row = $req7 -> fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT);
+        $personID2 = $row["Person_ID"];    
+    
 
-    include ("homeC.php");
+        
+    $req8 = $bdd->prepare("SELECT DATE_FORMAT(NOW(),'%c-%d-%Y %h:%i %p')", array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+    $req8->execute(array());
+        $row = $req8 -> fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT);
+        $date = $row["DATE_FORMAT(NOW(),'%c-%d-%Y %h:%i %p')"]; 
+        echo "$date";
+        
+    $req9 = $bdd->prepare('INSERT INTO Connexion (Last_Connexion) VALUE (:Last_Connexion)');
+    $req9->execute(array(
+        'Last_Connexion' => "$date",)); 
+        
+    $req10 = $bdd->prepare("SELECT Connexion_ID FROM Connexion WHERE (Last_Connexion = '$date')", array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+    $req10->execute(array());
+        $row = $req10 -> fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT);
+        $connexion = $row["Connexion_ID"];    
+        
+        
+    $req11 = $bdd->prepare('INSERT INTO Member (Member_Title, Member_Satus, District_ID, Club_ID, Member_Num, Member_Additional_Adress, Member_Postal_Code, Member_Street, Member_City, Member_Phone, Member_Mobile, Member_Email, Member_Position_Club, Member_Position_District, Member_By_Train, Member_Date_Train, Connexion_ID,Person_ID, Follower_ID, Member_Password ) VALUES (:civilite,:status,:districtID,:clubID,:num,:supad,:cp,:rue,:ville,:phone,:mobile,:email,:pclub,:pdistrict,:btrain,:htrain,:connexion,:personID2,:personID,:mdp)');
+    $req11->execute(array(
+        'civilite' => "$civilite",
+        'status' => "$stauts",
+        'districtID' => "$districtID",
+        'clubID' => "$clubID",
+        'num' => "$num",
+        'supad' => "$supad",
+        'cp' => "$cp",
+        'rue' => "$rue",
+        'ville' => "$ville",
+        'phone' => "$tel",
+        'mobile' => "$portable",
+        'email' => "$email",
+        'pclub' => "$fClub",
+        'pdistrict' => "$fDistrict",
+        'btrain' => "$train",
+        'htrain' => "$traindate",
+        'connexion' => "$connexion",
+        'civilite' => "$civilite",
+        'personID2' => "$personID2",
+        'personID' => "$personID",
+        'mdp' => "$mdp",));      
+        
+        
+            
+        
+         
+        
+
+        
+        
+
+    //include ("homeC.php");
 }
 ?>
